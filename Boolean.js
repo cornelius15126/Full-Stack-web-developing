28b00fe24df548bd08286f4d6d98c21c6e4f7afa
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-08 16:41:09 +0100

    Add files via upload
    boolean

---
+++ Boolean.js
@@ -0,0 +1,16 @@
+var shouldBeTrue = false;
+if (shouldBeTrue === true){
+    console.log("Hello");
+}
+else{
+    console.log("world");
+}
+
+var shouldBeTrue = true;
+var shouldBeNull = null;
+if (shouldBeTrue === true){
+    console.log("Hello");
+}
+else{
+    console.log("world");
+}        
