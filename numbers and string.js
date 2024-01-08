3a8683f03d2ea524274455ce0590886df9b1fe23
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-08 15:01:34 +0100

    Create numbers and string.js

---
+++ numbers and string.js
@@ -0,0 +1,10 @@
+console.log(108*5);
+console.log(Math.PI);
+console.log(typeof '1444');
+console.log(parseInt('172') + 34);
+console.log(isNaN('HELLO WORLD'));
+console.log(+'66');
+console.log('Abomination'. length);
+console.log('Cornelius'. charAt(3));
+console.log('hello' + 'world' + "!");
+console.log("Raise amm Raise amm!!!". toUpperCase());
