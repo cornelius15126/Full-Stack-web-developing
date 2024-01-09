4671955de3f23c6714884d499ac8eb0cda793d76
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-09 14:37:53 +0100

    uploaded file

---
+++ Array.js
@@ -0,0 +1,11 @@
+var mylist =["Adam","Rodney","Praise",'Ruby',"Victor"];
+console.log(mylist) ;
+console.log(mylist.length);
+var sortedArray = mylist.sort();
+console.log(sortedArray);
+var ReverseArray = mylist.reverse();
+console.log(ReverseArray);
+
+var newlist = ["James","Peter","Sharon",'Davide',"Promise"];
+var concattedArray = mylist.concat(newlist);
+console.log(concattedArray);
