68ccde653162d6c3a9b084197b7e7e55af4c06c5
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-10 14:12:14 +0100

    Add files via upload

---
+++ const.js
@@ -0,0 +1,15 @@
+function sayHi() {
+    let shouldSayHi = true;
+    const myName = 'Cornelius';
+    if (shouldSayHi === true){
+        myName = "Cornelius";
+        console.log("Hello, " + myName);
+        
+    }
+    else {
+        myName = "David";
+    
+console.log("Hello, " + myName);
+}
+}
+sayHi()
