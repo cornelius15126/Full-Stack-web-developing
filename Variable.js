46a54c0714e73b603e6c2b6ee15eb35d7329d056
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-10 10:46:12 +0100

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,10 @@
+var Name = "Cornelius";
+var age = 21;
+var location = "Edo State";
+var mysubject ={Name: "Cornelius", age: 21, location: "Edo" };
+console.log("my name is " + Name + ", i am " + age + " and i am from " +  location);
+console.log(mysubject);
+Name = "Nero"
+age= 35
+location="Calabar"
+console.log("my name is " + Name + ", i am " + age + " and i am from " + location);
