e36fa19f80b9cfc6d57f6a174a24b4017ba7785d
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-12 14:16:03 +0100

    Add files via upload

---
+++ Comparison operators.js
@@ -0,0 +1,10 @@
+const Bankbalance = 554;
+const costofgoods = 305;
+const tax = 0.90;
+
+if (Bankbalance === costofgoods + tax ){
+    console.log("I am not buying")
+}
+else{
+    console.log('I want to buy it')
+}
