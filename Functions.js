d40f27308e38d40e84e6468eadcff51c7870844d
cornelius15126 <109594951+cornelius15126@users.noreply.github.com>
2024-01-17 12:37:31 +0100

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,11 @@
+// functions is a javascript procedure that perform a tast or calculates a value
+// it takes in parameters which are values passed to the function when called.
+function sayHi(Firstname, Age) {
+    console.log("Hello " + ""+ Firstname + " You're "+ Age + " old");
+}
+
+
+ sayHi("Cornelius", 21);
+ sayHi("Promise", 26);
+ sayHi("David", 22);
+ 
